0a51d334113fd444e2365709e528e0330415ee48
qc200 <qc2003020403@163.com>
2023-03-25 17:02:32 +0800

    order-v1 @add 添加对应的数据库结构

---
+++ reggie-v1.sql
@@ -0,0 +1,337 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80032 (8.0.32)
+ Source Host           : localhost:3306
+ Source Schema         : reggie
+
+ Target Server Type    : MySQL
+ Target Server Version : 80032 (8.0.32)
+ File Encoding         : 65001
+
+ Date: 25/03/2023 17:00:08
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for address_book
+-- ----------------------------
+DROP TABLE IF EXISTS `address_book`;
+CREATE TABLE `address_book`  (
+  `id` bigint NOT NULL COMMENT '主键',
+  `user_id` bigint NOT NULL COMMENT '用户id',
+  `consignee` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '收货人',
+  `sex` varchar(2) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '性别',
+  `phone` varchar(11) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '手机号',
+  `about` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '定位地址，简化地址簿，这里直接是定位到门牌号那个',
+  `detail` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '详细地址,用户自定义输入的',
+  `label` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '标签',
+  `is_default` tinyint(1) NOT NULL DEFAULT 0 COMMENT '默认 0 否 1是',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `create_user` bigint NOT NULL COMMENT '创建人',
+  `update_user` bigint NOT NULL COMMENT '修改人',
+  `is_deleted` int NOT NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '地址管理' ROW_FORMAT = COMPACT;
+
+-- ----------------------------
+-- Table structure for category
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category`  (
+  `id` bigint NOT NULL COMMENT '主键',
+  `type` int NULL DEFAULT NULL COMMENT '类型   1 菜品分类 2 套餐分类',
+  `name` varchar(64) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '分类名称',
+  `sort` int NOT NULL DEFAULT 0 COMMENT '顺序',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `create_user` bigint NOT NULL COMMENT '创建人',
+  `update_user` bigint NOT NULL COMMENT '修改人',
+  `store_id` bigint NOT NULL COMMENT '所属哪个门店',
+  `version` int NOT NULL COMMENT '乐观锁',
+  `is_deleted` int NOT NULL COMMENT '假删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '菜品及套餐分类' ROW_FORMAT = COMPACT;
+
+-- ----------------------------
+-- Table structure for dish
+-- ----------------------------
+DROP TABLE IF EXISTS `dish`;
+CREATE TABLE `dish`  (
+  `id` bigint NOT NULL COMMENT '主键',
+  `name` varchar(64) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '菜品名称',
+  `category_id` bigint NOT NULL COMMENT '菜品分类id',
+  `price` decimal(10, 2) NOT NULL COMMENT '菜品价格',
+  `status` int NOT NULL DEFAULT 1 COMMENT '0 停售（看得见，显示停售） 1 起售',
+  `code` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '商品码，暂时没发现有什么用',
+  `description` varchar(400) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '描述信息',
+  `image` varchar(1000) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT '' COMMENT '图片',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `create_user` bigint NOT NULL COMMENT '创建人',
+  `update_user` bigint NOT NULL COMMENT '修改人',
+  `is_deleted` int NOT NULL DEFAULT 0 COMMENT '是否删除，1为假删除',
+  `version` int NOT NULL COMMENT '乐观锁version',
+  `store_id` bigint NOT NULL COMMENT '门店id',
+  `sort` int NOT NULL DEFAULT 0 COMMENT '顺序',
+  `sale_num` bigint NULL DEFAULT NULL COMMENT '销量',
+  `type` int NOT NULL DEFAULT 1 COMMENT 'dish/setmeal-->1/2',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_dish_name`(`name` ASC) USING BTREE,
+  INDEX `idx_dish_store_id`(`store_id` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '菜品管理' ROW_FORMAT = COMPACT;
+
+-- ----------------------------
+-- Table structure for dish_flavor
+-- ----------------------------
+DROP TABLE IF EXISTS `dish_flavor`;
+CREATE TABLE `dish_flavor`  (
+  `id` bigint NOT NULL COMMENT '主键',
+  `dish_id` bigint NOT NULL COMMENT '菜品',
+  `name` varchar(64) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '口味名称',
+  `value` varchar(500) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '口味数据list',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `create_user` bigint NOT NULL COMMENT '创建人',
+  `update_user` bigint NOT NULL COMMENT '修改人',
+  `is_deleted` int NOT NULL DEFAULT 0 COMMENT '是否删除,0为未删除，1为删除',
+  `version` int NULL DEFAULT NULL COMMENT '乐观锁，无效字段,可以删除，口味不单独判断版本',
+  `store_id` bigint NOT NULL COMMENT '门店id，属于哪个门店的甜辣度',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_dish_id`(`dish_id` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '菜品口味关系表' ROW_FORMAT = COMPACT;
+
+-- ----------------------------
+-- Table structure for employee
+-- ----------------------------
+DROP TABLE IF EXISTS `employee`;
+CREATE TABLE `employee`  (
+  `id` bigint NOT NULL COMMENT '主键',
+  `name` varchar(32) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '姓名',
+  `username` varchar(32) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '密码',
+  `salt` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '盐',
+  `phone` varchar(11) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '手机号',
+  `sex` varchar(2) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '性别',
+  `id_number` varchar(18) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '身份证号',
+  `status` int NOT NULL DEFAULT 1 COMMENT '状态 0:禁用，1:正常',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `create_user` bigint NOT NULL COMMENT '创建人',
+  `update_user` bigint NOT NULL COMMENT '修改人',
+  `is_deleted` int(1) UNSIGNED ZEROFILL NOT NULL COMMENT '逻辑删除,0未删除，1删除',
+  `permissions` int NOT NULL COMMENT '权限绑定',
+  `store_id` bigint NULL DEFAULT NULL COMMENT '3，4才需要\r\n1，2为所有门店的管理员，3为指定门店管理员，4为门店员工，其中1为admin（拥有操作所有用户的权限）',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `idx_username`(`username` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '员工信息' ROW_FORMAT = COMPACT;
+
+-- ----------------------------
+-- Table structure for menu
+-- ----------------------------
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE `menu`  (
+  `id` bigint NOT NULL,
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '路由路径',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '路由name',
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '中文标题',
+  `component` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '组件名',
+  `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'icon',
+  `requireAuth` int NOT NULL COMMENT '是否需要登录，1need',
+  `enabled` int NOT NULL COMMENT '是否启用,1为启用，0false',
+  `roles` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '逗号分割，允许哪些role_id访问，这个必须需要登录',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for order_detail
+-- ----------------------------
+DROP TABLE IF EXISTS `order_detail`;
+CREATE TABLE `order_detail`  (
+  `id` bigint NOT NULL COMMENT '主键',
+  `name` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '名字',
+  `image` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '图片',
+  `order_id` bigint NOT NULL COMMENT '订单id',
+  `type` int NOT NULL COMMENT 'type,0dish,1setmeal',
+  `dish_id` bigint NULL DEFAULT NULL COMMENT '菜品id',
+  `setmeal_id` bigint NULL DEFAULT NULL COMMENT '套餐id',
+  `dish_flavor` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '口味，直接以逗号隔开就行，包括选择的口味，和备注的特别要求',
+  `number` int NOT NULL DEFAULT 1 COMMENT '数量',
+  `amount` decimal(10, 2) NOT NULL COMMENT '金额',
+  `store_id` bigint NOT NULL COMMENT '订单归属指定门店,在订单表和订单详细表进行数据冗余操作',
+  `is_deleted` int NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '订单明细表' ROW_FORMAT = COMPACT;
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders`  (
+  `id` bigint NOT NULL COMMENT '订单号(订单id)',
+  `pay_status` int NOT NULL DEFAULT 1 COMMENT '订单支付状态 1待付款，2已经支付',
+  `user_id` bigint NOT NULL COMMENT '下单用户',
+  `address_book_id` bigint NOT NULL COMMENT '地址id',
+  `order_time` datetime NOT NULL COMMENT '下单时间',
+  `checkout_time` datetime NULL DEFAULT NULL COMMENT '结账时间',
+  `pay_method` int NULL DEFAULT 1 COMMENT '支付方式 1微信,2支付宝',
+  `amount` decimal(10, 2) NOT NULL COMMENT '实收金额',
+  `remark` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '备注',
+  `phone` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
+  `address` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
+  `consignee` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '收货人',
+  `store_id` bigint NOT NULL COMMENT '属于哪个分店',
+  `is_deleted` int NOT NULL COMMENT '0为未删除',
+  `status_remark` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '状态备注',
+  `order_status` int NOT NULL COMMENT '1，未支付2，待派送，3已派送，4已完成，5已取消',
+  `version` int NOT NULL COMMENT '乐观锁',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '订单表' ROW_FORMAT = COMPACT;
+
+-- ----------------------------
+-- Table structure for setmeal
+-- ----------------------------
+DROP TABLE IF EXISTS `setmeal`;
+CREATE TABLE `setmeal`  (
+  `id` bigint NOT NULL COMMENT '主键',
+  `category_id` bigint NOT NULL COMMENT '菜品\\套餐分类id',
+  `name` varchar(66) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '套餐名称',
+  `price` decimal(10, 2) NOT NULL COMMENT '套餐价格',
+  `status` int NOT NULL COMMENT '状态 0:停用 1:启用',
+  `code` varchar(32) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '编码？？套餐码？',
+  `description` varchar(300) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '描述信息',
+  `image` varchar(600) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '图片',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  `create_user` bigint NOT NULL COMMENT '创建人',
+  `update_user` bigint NOT NULL COMMENT '修改人',
+  `is_deleted` int NOT NULL DEFAULT 0 COMMENT '是否删除',
+  `version` int NOT NULL COMMENT '乐观锁，套餐和菜品关系表不再绑定，关系表一般只会在更新套餐表时更新',
+  `store_id` bigint NOT NULL COMMENT '哪个门店的套餐(冗余数据，其实根据套餐分类就能找到分类id，再拿分类id找套餐)',
+  `sort` int NOT NULL COMMENT '排序',
+  `sale_num` bigint NULL DEFAULT NULL COMMENT '销量、、不为0',
+  `type` int NOT NULL DEFAULT 2 COMMENT 'dish/setmeal-->1/2',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_setmeal_store_id`(`store_id` ASC) USING BTREE,
+  INDEX `idx_setmeal_name`(`name` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '套餐' ROW_FORMAT = COMPACT;
+
+-- ----------------------------
+-- Table structure for setmeal_dish
+-- ----------------------------
+DROP TABLE IF EXISTS `setmeal_dish`;
+CREATE TABLE `setmeal_dish`  (
+  `id` bigint NOT NULL COMMENT '主键',
+  `setmeal_id` varchar(32) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '套餐id ，哪个套餐所绑定的',
+  `dish_id` varchar(32) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '菜品id，绑定菜品的id',
+  `name` varchar(32) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '菜品名称 （冗余字段）',
+  `price` decimal(10, 2) NULL DEFAULT NULL COMMENT '菜品原价（冗余字段）',
+  `image` varchar(500) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '菜品图片',
+  `copies` int NOT NULL COMMENT '份数',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  `create_user` bigint NOT NULL COMMENT '创建人',
+  `update_user` bigint NOT NULL COMMENT '修改人',
+  `is_deleted` int NOT NULL DEFAULT 0 COMMENT '是否删除',
+  `store_id` bigint NOT NULL COMMENT '商店id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '套餐菜品关系' ROW_FORMAT = COMPACT;
+
+-- ----------------------------
+-- Table structure for setmeal_flavor
+-- ----------------------------
+DROP TABLE IF EXISTS `setmeal_flavor`;
+CREATE TABLE `setmeal_flavor`  (
+  `id` bigint NOT NULL COMMENT '主键',
+  `setmeal_id` bigint NOT NULL COMMENT '菜品',
+  `name` varchar(64) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '口味名称',
+  `value` varchar(500) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '口味数据list',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `create_user` bigint NOT NULL COMMENT '创建人',
+  `update_user` bigint NOT NULL COMMENT '修改人',
+  `is_deleted` int NOT NULL DEFAULT 0 COMMENT '是否删除,0为未删除，1为删除',
+  `version` int NULL DEFAULT NULL COMMENT '乐观锁，无效字段,可以删除，口味不单独判断版本',
+  `store_id` bigint NOT NULL COMMENT '门店id，属于哪个门店的甜辣度',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_dish_id`(`setmeal_id` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '菜品口味关系表' ROW_FORMAT = COMPACT;
+
+-- ----------------------------
+-- Table structure for shopping_cart
+-- ----------------------------
+DROP TABLE IF EXISTS `shopping_cart`;
+CREATE TABLE `shopping_cart`  (
+  `id` bigint NOT NULL COMMENT '主键,name,iamge,price都是直接查，如果版本不匹配就提示用户已更新',
+  `user_id` bigint NOT NULL COMMENT '用户id,用于标记谁的购物车',
+  `type` int NOT NULL COMMENT '0：dish，1：setmeal',
+  `dish_id` bigint NULL DEFAULT NULL COMMENT '菜品id//可能是菜品，也可能是套餐',
+  `setmeal_id` bigint NULL DEFAULT NULL COMMENT '套餐id',
+  `dish_flavor` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '口味，字符串即可，不过这样就导致了购物车不可以编辑配置，只能删除再次添加，创建订单时附带上订单提交时的备注',
+  `number` int NOT NULL DEFAULT 1 COMMENT '数量',
+  `create_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  `store_id` bigint NOT NULL COMMENT '商店id',
+  `version` int NOT NULL COMMENT '版本号,用来看菜品或者套餐是否已经被后台更新过了',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '购物车' ROW_FORMAT = COMPACT;
+
+-- ----------------------------
+-- Table structure for store
+-- ----------------------------
+DROP TABLE IF EXISTS `store`;
+CREATE TABLE `store`  (
+  `store_id` bigint NOT NULL COMMENT '门店Id,雪花算法生成mp',
+  `store_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '门店名称',
+  `store_introduction` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '门店简介',
+  `is_deleted` int NOT NULL COMMENT '0为未删除，1为删除',
+  `store_status` int NOT NULL COMMENT '0为休息中，1为正在营业',
+  `create_time` datetime NOT NULL COMMENT '创建店的时间',
+  `update_time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新店的时间',
+  `create_user` bigint NOT NULL COMMENT '创建店的用户id',
+  `update_user` bigint NOT NULL COMMENT '更新店的用户id',
+  PRIMARY KEY (`store_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for transaction_flow
+-- ----------------------------
+DROP TABLE IF EXISTS `transaction_flow`;
+CREATE TABLE `transaction_flow`  (
+  `id` bigint NOT NULL COMMENT '流水id',
+  `subject` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '生成的交易name',
+  `trade_status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '第三方返回的交易状态',
+  `trade_no` bigint NOT NULL COMMENT '交易流水号(重要)',
+  `out_trade_no` bigint NOT NULL COMMENT '商户订单号(对应订单id)',
+  `amount` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '交易金额',
+  `buyer_id` bigint NOT NULL COMMENT '买家在支付宝唯一id',
+  `gmt_payment` datetime NULL DEFAULT NULL COMMENT '买家付款时间',
+  `buyer_pay_amount` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '买家付款金额',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` bigint NOT NULL COMMENT '主键',
+  `name` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '姓名,若为空的话就随机生成一个',
+  `phone` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL COMMENT '手机号//没钱搞短信验证，这里暂时使用控制台输出验证码并且存入redis,key作为手机号，一个手机号同时只会存在一个没有过期的验证码',
+  `sex` varchar(2) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '性别',
+  `id_number` varchar(18) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '身份证号',
+  `avatar` varchar(500) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NULL DEFAULT NULL COMMENT '头像',
+  `status` int NOT NULL DEFAULT 0 COMMENT '状态 0:禁用，1:正常',
+  `is_deleted` int NOT NULL COMMENT '是否删除,逻辑删除--1为删除，0为未删除',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_user_id`(`id` ASC) USING BTREE,
+  INDEX `idx_user_phone`(`phone` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_bin COMMENT = '用户信息' ROW_FORMAT = COMPACT;
+
+SET FOREIGN_KEY_CHECKS = 1;
