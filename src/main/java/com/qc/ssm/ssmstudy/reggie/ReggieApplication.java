4d0f3cadd14195a98a8cd91cf905471c37f5a5d7
qc200 <qc2003020403@163.com>
2023-04-01 00:05:58 +0800

    @add 添加对应的数据库结构 @Version 1.1 @add swagger升级为3

--- src/main/java/com/qc/ssm/ssmstudy/reggie/ReggieApplication.java
+++ src/main/java/com/qc/ssm/ssmstudy/reggie/ReggieApplication.java
@@ -4,7 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
+
+@EnableOpenApi
 @Slf4j
 @SpringBootApplication
 //@MapperScan("com.qc.ssm.ssmstudy.reggie.mapper"),也可以在每个mapper接口上加上@mapper注解
--- src/main/java/com/qc/ssm/ssmstudy/reggie/config/WebMvcConfig.java
+++ src/main/java/com/qc/ssm/ssmstudy/reggie/config/WebMvcConfig.java
@@ -5,12 +5,10 @@ import com.qc.ssm.ssmstudy.reggie.common.JwtTokenInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.*;
 
 import java.util.List;
 
@@ -27,9 +25,9 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 解决swagger无法访问
-        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/swagger-ui/**").addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
         // 解决swagger的js文件无法访问
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+//        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 
         log.info("开始静态资源映射");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");//此处classpath:/front/必须得在末尾加/，否则无法访问
@@ -46,7 +44,7 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         ir.addPathPatterns("/**");
 
         // 不拦截路径，如：注册、登录、忘记密码等
-        ir.excludePathPatterns("/employee/login","/employee/logout","/alipay/**");//可以直接逗号往后加
+        ir.excludePathPatterns("/employee/login","/employee/logout","/alipay/**","/swagger-ui/**");//可以直接逗号往后加
     }
     /**
      * 扩展mvc框架的消息转换器
@@ -61,5 +59,9 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         //将上面的消息转换器对象追加到mvc框架的转换器集合中
         converters.add(0,messageConverter);
     }
-
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/swagger-ui", "/swagger-ui/index.html") .setStatusCode(HttpStatus.TEMPORARY_REDIRECT);
+        super.addViewControllers(registry);
+    }
 }
